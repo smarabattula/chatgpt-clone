e3001ce8643108a7a18648104e15c5bb34a0d991
smaraba <smaraba@ncsu.edu>
2024-08-25 00:13:42 -0700

    init sidebar template

---
+++ frontend-client/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import './normal.css';
+function App() {
+  return (
+    <div className="App">
+      <aside className = "sidemenu">
+        <div className = "sidemenu-button">
+          <span>
+            +
+          </span>
+          New Chat
+        </div>
+      </aside>
+      <section className = "chatbox">
+
+      </section>
+
+    </div>
+  );
+}
+
+export default App;
