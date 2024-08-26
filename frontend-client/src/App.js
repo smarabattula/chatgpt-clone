4925dd2c9132055a5d0b00b402ac45af7a4aa157
smaraba <smaraba@ncsu.edu>
2024-08-25 22:30:05 -0700

    chat messages

--- frontend-client/src/App.js
+++ frontend-client/src/App.js
@@ -5,13 +5,26 @@ function App() {
     <div className="App">
       <aside className = "sidemenu">
         <div className = "sidemenu-button">
-          <span>
-            +
-          </span>
+          <span> + </span>
           New Chat
         </div>
       </aside>
       <section className = "chatbox">
+        <div className = "chat-msg chatgpt">
+          <div className = "avatar">
+          </div>
+          <div className = "message chat-log">
+          Welcome! I am ChatGPT response.
+          </div>
+        </div>
+
+        <div className = "chat-msg">
+          <div className = "avatar">
+          </div>
+          <div className = "message">
+          Hello World! I am human prompt.
+          </div>
+        </div>
       <div className = "chat-input-box">
       <textarea className = "chat-input-textarea" placeholder = "Type your message here"></textarea>
       </div>
