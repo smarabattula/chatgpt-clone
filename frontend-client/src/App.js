85711978c6e64597dafcc1fa8244946132ee6395
smaraba <smaraba@ncsu.edu>
2024-08-26 01:39:29 -0700

    added react state

--- frontend-client/src/App.js
+++ frontend-client/src/App.js
@@ -1,7 +1,27 @@
 import './App.css';
 import './normal.css';
 import ChatGPTAvatar from './ChatGPTAvatar';
+import {useState } from "react";
 function App() {
+
+  // Add state for input and chat log
+
+  const [input, setInput] = useState("");
+  const [chatLog, setChatLog] = useState([{
+    user: "gpt",
+    message: "How can I help you today?"
+  },
+  {
+    user: "me",
+    message: "Hello World! I'm human prompt"
+  }]);
+  async function handleSubmit(e){
+    e.preventDefault();
+    setChatLog([...chatLog, {user: "me", message:`${input}
+      `}])
+    setInput("");
+    // console.log("Kukka submit");
+  }
   return (
     <div className="App">
       <aside className = "sidemenu">
@@ -11,27 +31,16 @@ function App() {
         </div>
       </aside>
       <section className = "chatbox">
-
-        <div className = "chat-msg">
-          <div className = "avatar">
-          </div>
-          <div className = "message">
-          Hello World! I am human prompt.
-          </div>
-        </div>
-
-        <div className = "chat-msg chatgpt">
-          <div className = "avatar">
-          <ChatGPTAvatar />
-          </div>
-          <div className = "message chat-log">
-          Welcome! I am ChatGPT response.
-          </div>
-        </div>
+        {chatLog.map((message, index) => (
+        <ChatMessage key = {index} message = {message}/>
+        ))}
 
       <div className = "chat-input-box">
-
-      <textarea className = "chat-input-textarea" placeholder = "Type your message here"></textarea>
+      <form onSubmit={handleSubmit}>
+      <input rows = "1"
+      value = {input}
+      onChange = {(e) => setInput(e.target.value)} className = "chat-input-textarea" placeholder = "Type your message here"></input>
+      </form>
       </div>
       </section>
 
@@ -40,3 +49,16 @@ function App() {
 }
 
 export default App;
+
+const ChatMessage = ({message}) => {
+  return (
+    <div className = {`chat-msg ${message.user == "gpt" && "chatgpt"}`}>
+    <div className = {`avatar ${message.user == "gpt" && "chatgpt"}`}>
+    </div>
+    {message.user == "gpt" && <ChatGPTAvatar/>}
+    <div className = "message">
+    {message.message}
+    </div>
+  </div>
+  )
+}
